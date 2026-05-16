80bcdf50df50f0c2cc7a9e7083fe249ddc724fd7
karinapinotti <karina-bruschi@hotmail.com>
2026-05-16 10:44:41 -0300

    feat: integração com jpa

---
+++ src/main/java/br/com/pinotti/events/model/SubscriptionID.java
@@ -0,0 +1,35 @@
+package br.com.pinotti.events.model;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+@Embeddable
+public class SubscriptionID implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "subscribed_user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "session_id")
+    private Session session;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+}
